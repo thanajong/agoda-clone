54bc023648db8977ece3e73a6de28886e7b3f87f
Thanapat Jonglertthiti <thanajong@outlook.com>
2024-09-16 00:04:30 +0700

    add font Helvetica Neue Medium

--- frontend/src/components/TileHeader.jsx
+++ frontend/src/components/TileHeader.jsx
@@ -4,7 +4,12 @@ const TileHeader = () => {
 	return (
 		<section class='tile-container-section'>
 			<div id='background-image-wrapper'>
-				<div class='background'></div>
+				<div class='background'>
+				</div>
+				<section class='tile-container-column'>
+					<div>
+					</div>	
+				</section>
 			</div>
 		</section>
 	);
