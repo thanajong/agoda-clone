f2b0767785fc9791b275f6ffb2ca1ca27043736f
Thanapat Jonglertthiti <thanajong@outlook.com>
2024-09-16 01:34:53 +0700

    add TileHeader welcome banner

--- frontend/src/components/TileHeader.jsx
+++ frontend/src/components/TileHeader.jsx
@@ -6,11 +6,19 @@ const TileHeader = () => {
 			<div id='background-image-wrapper'>
 				<div class='background'>
 				</div>
-				<section class='tile-container-column'>
-					<div>
-					</div>	
-				</section>
-			</div>
+			</div>	
+			<section class='tile-container-column'>
+				<div class='tile-container-wrapper'>
+					<div class='welcome-message-banner'>
+						<div class='welcome-message-wrapper'>
+							<h1 class='banner'>See the world for less</h1>
+							<h2></h2>
+						</div>
+					</div>
+					<div class='booking-box'>
+				</div>
+				</div>
+			</section>		
 		</section>
 	);
 }
