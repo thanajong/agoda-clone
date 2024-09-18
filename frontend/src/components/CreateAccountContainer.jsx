bb980984d796bc9d351abb23ee09f3d1ce906aef
Thanapat Jonglertthiti <thanajong@outlook.com>
2024-09-18 16:14:03 +0700

    add component CreateAccountContainer

---
+++ frontend/src/components/CreateAccountContainer.jsx
@@ -0,0 +1,13 @@
+import './CreateAccountContainer.css';
+
+const CreateAccountContainer = () => {
+    return (
+        <section id='create-account'>
+            <div className="create-account-container">
+                test
+            </div>
+        </section>
+    );
+}
+
+export default CreateAccountContainer;
--- frontend/src/pages/CreateAccount.jsx
+++ frontend/src/pages/CreateAccount.jsx
@@ -1,9 +1,11 @@
 import NewHeader from "../components/NewHeader";
+import CreateAccountContainer from "../components/CreateAccountContainer";
 
 const CreateAccount = () => {
     return (
         <>
             <NewHeader />
+            <CreateAccountContainer />
         </>
     )
 }
