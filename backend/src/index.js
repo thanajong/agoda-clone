5ef30cf687c0bb27d9e70248ce00997723115a5c
thanajong <thanajong@outlook.com>
2024-09-19 17:58:47 +0700

    backend port 4000

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import authRoutes from './routes/auth.routes.js'
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json());    // allows to parse incoming requests : req.body
