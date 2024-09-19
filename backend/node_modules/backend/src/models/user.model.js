b38c0a3921345a0a2422df20bc0da66dd5fe8b67
thanajong <thanajong@outlook.com>
2024-09-19 16:38:03 +0700

    sign out endpoint

---
+++ backend/node_modules/backend/src/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    verificationToken: String,
+    verificationTokenExpiresAt: Date,
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+export default User;
