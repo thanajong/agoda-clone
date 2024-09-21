fdd0e6bc6656dbd61cd5bce5bc694f59377d4382
thanajong <thanajong@outlook.com>
2024-09-22 03:37:27 +0700

    api-client import .env VITE_API_BASE_URL

--- frontend/src/api-client/api-client.js
+++ frontend/src/api-client/api-client.js
@@ -1,8 +1,7 @@
-// const API_BASE_URL = import.meta.env.API_BASE_URL;   fix later, not importing from .env
-const API_BASE_URL = 'http://localhost:4000'
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const createAccount = async (formData) => {
-    const response = await fetch(`${API_BASE_URL}/api/auth/create-account`, {
+    const response = await fetch(`${VITE_API_BASE_URL}/api/auth/create-account`, {
 
         method: "POST",
 
